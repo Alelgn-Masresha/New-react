216996a6e77cd6dbe3b72c1ecbcbdd26a4cd5c79
Alelgn-Masresha <alxmax071@gmail.com>
2024-05-27 14:23:45 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Left from "./components/Left";
+import { HeroParallaxDemo } from "./components/ui/Para";
+
+function App() {
+  return (
+    <section>
+      <Left />
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/Left.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeroParallaxDemo } from "./ui/Para";
+
+const Left = () => {
+  return (
+    <div className="left">
+      <HeroParallaxDemo />
+    </div>
+  );
+};
+
+export default Left;
